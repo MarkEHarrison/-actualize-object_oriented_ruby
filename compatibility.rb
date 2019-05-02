9b49a112cb73281225fb2c8726cccbf7c7bdd952
Mark Harrison <mhworld11@icloud.com>
2019-05-02 10:44:09 -0700

    makes use of require method

---
+++ compatibility.rb
@@ -0,0 +1,23 @@
+
+
+module Compatibility
+
+  
+  
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
